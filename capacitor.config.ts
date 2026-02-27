ff438822e6f06ac51460aa1c56079303206bbf36
gaelGn10 <rambelosongael@gmail.com>
2026-02-27 16:21:32 +0300

    Fix Capacitor webDir and add Android platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'ionic android aok',
+  webDir: 'dist'
+};
+
+export default config;
